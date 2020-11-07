d3e86bf93a266af4bdf462b67d840f6d76fa1519
renato <renatomacegossa@gmail.com>
2020-11-07 01:22:48 -0300

    spring retry comentado

--- src/main/java/br/com/rmd/demo/dynamo/Application.java
+++ src/main/java/br/com/rmd/demo/dynamo/Application.java
@@ -2,7 +2,9 @@ package br.com.rmd.demo.dynamo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
 @SpringBootApplication
 public class Application {
 
--- src/main/java/br/com/rmd/demo/dynamo/repository/RegistraEventoRepository.java
+++ src/main/java/br/com/rmd/demo/dynamo/repository/RegistraEventoRepository.java
@@ -4,15 +4,22 @@ import br.com.rmd.demo.dynamo.model.Analitico;
 import br.com.rmd.demo.dynamo.model.Consolidado;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.TransactionWriteRequest;
+import com.amazonaws.services.dynamodbv2.model.ConditionalCheckFailedException;
+import com.amazonaws.services.dynamodbv2.model.TransactionCanceledException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Repository;
 
+@Slf4j
 @Repository
 public class RegistraEventoRepository {
 
     @Autowired
     private DynamoDBMapper dynamoDBMapper;
 
+//    @Retryable( value = { ConditionalCheckFailedException.class, TransactionCanceledException.class }, maxAttempts = 2 )
     public void registrar(Analitico analitico) {
 
         TransactionWriteRequest transactionWriteRequest = new TransactionWriteRequest();
@@ -42,4 +49,14 @@ public class RegistraEventoRepository {
 
         dynamoDBMapper.transactionWrite( transactionWriteRequest );
     }
+
+//    @Recover
+//    void recover(ConditionalCheckFailedException e, Analitico analitico) {
+//        log.error( String.format( "caiu no retry. %s", analitico ) );
+//    }
+//
+//    @Recover
+//    void recover(TransactionCanceledException e, Analitico analitico) {
+//        log.error( String.format( "caiu no retry. %s", analitico ) );
+//    }
 }
